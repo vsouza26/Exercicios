553dfe873f1cd7d412e20d2e01f6fafeb4819dde
Prof Christina Flach <71397552+christinaflachufba@users.noreply.github.com>
2022-10-24 13:08:24 -0300

    Update main.c

--- E7/main.c
+++ E7/main.c
@@ -2,10 +2,13 @@
 
 extern int yyparse();
 
+// definir função expr_print
+
+
 int main() {
     if (yyparse()==0)
        // printf("Parse successful!\n"); 
-       // substituir essa mensagem pelo valor da expressão interpretada.
+       // substituir essa mensagem pela chamada à função expr_print.
     else 
        printf("Parse failed.\n"); 
 }
